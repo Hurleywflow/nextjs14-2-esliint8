366cd5bdb94c75a2e9c7139a549cfe57beafe5c4
Hurley <hurleywflow@gmail.com>
2024-05-13 10:30:59 +1000

    style: :art: <Suspense fallback={<Loading className='h-fit min-h-screen' />}>

--- app/page.tsx
+++ app/page.tsx
@@ -15,37 +15,37 @@ const Home = (): JSX.Element => {
 	// 		setTimeout(resolve, 5000);
 	// 	});
 	return (
-		<main className='m-0 h-fit w-full p-0 '>
+		<main className='m-0 h-fit w-full overflow-hidden p-0'>
 			<Container>
 				<div className='relative w-full'>
-					<div className='sticky top-0 flex h-fit min-h-screen items-center justify-center bg-red-400'>
-						<Suspense fallback={<Loading />}>
+					<div className='sticky top-0 flex h-fit min-h-screen w-full items-center justify-center bg-red-400'>
+						<Suspense fallback={<Loading className='h-fit min-h-screen' />}>
 							<Carousel />
 						</Suspense>
 					</div>
 
-					<div className='sticky top-0 flex h-fit min-h-screen items-center justify-center bg-blue-400'>
-						<Suspense fallback={<Loading />}>
+					<div className='sticky top-0 flex h-fit min-h-screen w-full items-center justify-center bg-blue-400'>
+						<Suspense fallback={<Loading className='h-fit min-h-screen' />}>
 							<SkewScroll />
 						</Suspense>
 					</div>
-					<div className='sticky top-0 flex h-fit min-h-screen items-center justify-center bg-blue-400'>
-						<Suspense fallback={<Loading />}>
+					<div className='sticky top-0 flex h-fit min-h-screen w-full items-center justify-center bg-blue-400'>
+						<Suspense fallback={<Loading className='h-fit min-h-screen' />}>
 							<CarouselOrientation />
 						</Suspense>
 					</div>
-					<div className='sticky top-0 flex h-fit min-h-screen items-center justify-center bg-blue-400'>
-						<Suspense fallback={<Loading />}>
+					<div className='sticky top-0 flex h-fit min-h-screen w-full items-center justify-center bg-blue-400'>
+						<Suspense fallback={<Loading className='h-fit min-h-screen' />}>
 							<VerticalParallaxTiltScroll />
 						</Suspense>
 					</div>
-					<div className='sticky top-0 flex h-fit min-h-screen items-center justify-center bg-blue-400'>
-						<Suspense fallback={<Loading />}>
+					<div className='sticky top-0 flex h-fit min-h-screen w-full items-center justify-center bg-blue-400'>
+						<Suspense fallback={<Loading className='h-fit min-h-screen' />}>
 							<InfiniteMovingCardsDemo />
 						</Suspense>
 					</div>
-					<div className='sticky top-0 flex h-fit min-h-screen items-center justify-center bg-blue-400'>
-						<Suspense fallback={<Loading />}>
+					<div className='sticky top-0 flex h-fit min-h-screen w-full items-center justify-center bg-blue-400'>
+						<Suspense fallback={<Loading className='h-fit min-h-screen' />}>
 							<CarouselWithThumbnails />
 						</Suspense>
 					</div>
